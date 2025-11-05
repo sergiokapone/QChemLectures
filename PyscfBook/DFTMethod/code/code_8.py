29ac27d3a4ebbd2e1b0bf961d3beea63592362ff
sergiokapone <sergiy.ponomarenko@gmail.com>
2025-11-05 18:38:09 +0200

    stop DFT

--- PyscfBook/DFTMethod/code/code_8.py
+++
@@ -1,9 +0,0 @@
-from pyscf import gto, dft
-
-mol = gto.M(atom="C 0 0 0", basis="def2-qzvp", spin=2)
-
-mf = dft.UKS(mol)
-mf.xc = "scan"
-energy_scan = mf.kernel()
-
-print(f"Енергія C (SCAN): {energy_scan:.8f} Ha")
